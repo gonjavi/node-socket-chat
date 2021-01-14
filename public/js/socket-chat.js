3c00e2c99106115750e940895ed92cce150327f8
manci <javiercaliescali@gmail.com>
2021-01-13 21:18:07 -0500

    enviar mensaje a todos

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -28,13 +28,13 @@ socket.on('disconnect', function() {
 
 
 // Enviar información
-socket.emit('enviarMensaje', {
+/* socket.emit('enviarMensaje', {
     usuario: 'Fernando',
     mensaje: 'Hola Mundo'
 }, function(resp) {
     console.log('respuesta server: ', resp);
 });
-
+ */
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
     console.log('Servidor:', mensaje);
